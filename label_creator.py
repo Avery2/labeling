063e33ab1187501fd45b8d1bbf3e1eb2fbac51e4
Avery Chan <JustinAveryChan@gmail.com>
2022-01-12 23:12:29 -0600

    refactor before implement undo

--- label_creator.py
+++ label_creator.py
@@ -1,4 +1,3 @@
-import time
 import sys
 import cv2 as cv
 import csv
@@ -9,51 +8,52 @@ def moveleft(n=1000):
     return u"\u001b[{}D".format(n)
 
 
-def readFrame(cap, IMG_SIZE=(128, 128)):
+def readFrame(cap):
     '''
     Reads the next frame using cap from cv2.VideoCapture
     :param cap: cv2.VideoCapture instance
-    :param IMG_SIZE: size of the returned image
     :return: Returns frame and its gray analog
     '''
 
-    ret, origFrame = cap.read()
+    ret, frame = cap.read()
     if not ret:
-        return ret, None, None, None
-    # frame = cv.resize(origFrame, IMG_SIZE)
-    # gray = cv.cvtColor(origFrame, cv.COLOR_BGR2GRAY)
-    # cv.imshow("origframe", origFrame)
+        return ret, None
 
-    return ret, origFrame, origFrame, origFrame
-
-labels = []
-frame_i = 0
-FILENAME = "SideView"
-VIDEO_PATH = f"data/{FILENAME}.mp4"
-OUTPUT_CSV_PATH = f"labels/{FILENAME}.csv"
+    return ret, frame
 
 if __name__ == '__main__':
+    labels = []
+    frame_i = 0
+    FILENAME = "SideView"
+    VIDEO_PATH = f"data/{FILENAME}.mp4"
+    OUTPUT_CSV_PATH = f"labels/{FILENAME}.csv"
     cap = cv.VideoCapture(VIDEO_PATH)
-    start = time.time()
+    haveNextFrame, frame = cap.read()
 
     # iterate over every frame
     while(cap.isOpened()):
-        ret, origFrame, _, _ = readFrame(cap)
-        if not ret:
+        if not haveNextFrame:
             break
-        cv.imshow(f"input", origFrame)
+        cv.imshow(f"input", frame)
+
         # if set to 0 will only move forward when something is pressed
         pressedKey = cv.waitKey(0)
+
         sys.stdout.write(moveleft() + f"Pressed key {chr(pressedKey & 0xFF)} {frame_i=}")
         sys.stdout.flush()
+
         if pressedKey & 0xFF == ord('q'):
             break
+        if pressedKey & 0xFF == ord('b'):
+            # TODO: implement undo
+            break
+        else:
+            labels.append(chr(pressedKey & 0xFF))
+            frame_i += 1
+            haveNextFrame, frame = readFrame(cap)
 
-        labels.append(chr(pressedKey & 0xFF))
-        frame_i += 1
-    end = time.time()
-    print(f"\nElapsed time: {round(end - start, 5)}s {frame_i=}")
     # print(" ".join(labels))
+
     # The following frees up resources and closes all windows
     cap.release()
     cv.destroyAllWindows()
