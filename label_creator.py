a660d62b46069c1fa775297556b15a3f77a5bd8e
roshanverma2001 <roshanverma01@gmail.com>
2022-03-26 15:20:16 -0500

    can iterate thru all videos and create csvs for them

--- label_creator.py
+++ label_creator.py
@@ -1,7 +1,8 @@
 import sys
 import cv2 as cv
 import csv
-
+import os
+import time
 
 def printOverwrite(s: str) -> None:
     """Prints something onn the same line, overwriting that was last written. Useful for a recurring value like a loading bar."""
@@ -14,6 +15,47 @@ def printOverwrite(s: str) -> None:
     sys.stdout.flush()
 
 
+def label_video( ARG_DICT, video_path: str, entity_video:str) -> None:
+    lc = LabelCreator(video_path)
+    haveNextFrame, frame = lc.cap.read()
+
+
+    # # iterate over every frame
+    #  # TODO: This isn't a great loop invariant now because cap doesn't get closed until the program terminates, so the program now relies on haveNextFrame being false to break out of loop.
+    while(lc.cap.isOpened()): 
+        if not haveNextFrame:
+            break
+        cv.imshow(f"input", frame)
+        pressedKey = cv.waitKey(0)  # if set to 0 will only move forward when something is pressed
+        printOverwrite(f"Pressed key {chr(pressedKey & 0xFF)} {lc.frameNum} {len(lc.labels)}")
+
+        # handle keypress
+        if pressedKey & 0xFF == ord('q'):
+            break
+        elif pressedKey & 0xFF == ord('b'):
+            haveNextFrame, frame = lc.undo()
+        else:
+            lc.labelFrame(pressedKey)
+            haveNextFrame, frame = lc.nextFrame()
+
+
+    session_name = ARG_DICT["output_directory"].split("/")[-1]
+
+    entity_name = entity_video.split(".")[0]
+
+    output_csv_dir_path = os.path.join("/".join(ARG_DICT["output_directory"].split("/")[:-1]), "LABELS_CSV",session_name, ARG_DICT["movement_name"],entity_name)
+
+    if not os.path.exists(output_csv_dir_path):
+        os.makedirs(output_csv_dir_path)
+
+    output_csv_path = os.path.join(output_csv_dir_path, str(lc.start_time)+".csv")
+    # write data
+    with open(output_csv_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(("Frame", "Label"))
+        for i, label in enumerate(lc.labels):
+            writer.writerow((i + 1, label))
+
 class LabelCreator:
     """Creates label for video."""
 
@@ -24,6 +66,7 @@ class LabelCreator:
         self.frameNum = 0
         self.buffSize = buffSize
         self.cap = cv.VideoCapture(videoPath)
+        self.start_time = time.time()
 
     def readFrame(self):
         """Reads and returns the next frame using cap from cv2.VideoCapture"""
@@ -65,42 +108,3 @@ class LabelCreator:
         self.cap.release()
         cv.destroyAllWindows()
 
-
-if __name__ == '__main__':
-    VIDEO_PATH_DEFAULT = "videos/pushups/SideView.mp4"
-    OUTPUT_CSV_PATH_DEFAULT = "labels/SideView.csv"
-
-    # command line parsing
-    if len(sys.argv) < 3:
-        print("usage: label_creator.py video_path output_csv_path")
-        exit(1)
-
-    video_path = sys.argv[1] or VIDEO_PATH_DEFAULT
-    output_csv_path = sys.argv[2] or OUTPUT_CSV_PATH_DEFAULT
-
-    lc = LabelCreator(video_path)
-    haveNextFrame, frame = lc.cap.read()
-
-    # iterate over every frame
-    while(lc.cap.isOpened()):  # TODO: This isn't a great loop invariant now because cap doesn't get closed until the program terminates, so the program now relies on haveNextFrame being false to break out of loop.
-        if not haveNextFrame:
-            break
-        cv.imshow(f"input", frame)
-        pressedKey = cv.waitKey(0)  # if set to 0 will only move forward when something is pressed
-        printOverwrite(f"Pressed key {chr(pressedKey & 0xFF)} {lc.frameNum=:04} {len(lc.labels)=:04}")
-
-        # handle keypress
-        if pressedKey & 0xFF == ord('q'):
-            break
-        elif pressedKey & 0xFF == ord('b'):
-            haveNextFrame, frame = lc.undo()
-        else:
-            lc.labelFrame(pressedKey)
-            haveNextFrame, frame = lc.nextFrame()
-
-    # write data
-    with open(output_csv_path, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(("Frame", "Label"))
-        for i, label in enumerate(lc.labels):
-            writer.writerow((i + 1, label))
